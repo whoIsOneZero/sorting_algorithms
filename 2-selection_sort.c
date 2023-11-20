33dcb99df4e5bc7ced1638567bda1b3e6724b1f9
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-20 10:41:31 -0800

    docs: insertion sort - array

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of (int), Selection sort
+ * @array - the array to be sorted
+ * @size - the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp, min_pos = 0;
+	size_t a, b;
+
+	/*Does not sort an array with <2 elements*/
+	for (a = 0; a < size - 1; a++)
+	{
+		min_pos = a;
+
+		/*Start sorting from the element after the current ...*/
+		/*... element at `min_pos`*/
+		for (b = a + 1; b < size; b++)
+		{
+			if (array[b] < array[min_pos])
+				min_pos = b;
+		}
+
+		if (min_pos != (int) a) /*Cases where a swap isn't necessary*/
+		{
+			temp = array[a];
+			array[a] = array[min_pos];
+			array[min_pos] = temp;
+		}
+	}
+}
