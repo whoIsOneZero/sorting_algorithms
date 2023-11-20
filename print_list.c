579ba6294738f62c898dc7d65da452c41b935fde
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-20 04:27:35 -0800

    add: helper files

---
+++ print_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+
