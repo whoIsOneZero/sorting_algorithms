03db7e43c9406f3d065234a9fa582280386bc20b
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-21 23:34:45 +0000

    tast 1 - insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,52 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * nodes_switch - Swap two nodes in a listint_t doubly-linked list.
+ * in descending order. smaller elements to the left.
+ *
+ * @head: A pointer to the head of the doubly-linked list.
+ * @big: a ptr to the node with larger `n` (data).
+ * @small: a ptr to the node with smaller `n` (data).
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void nodes_switch(listint_t **head, listint_t **big, listint_t *small)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*big)->next = small->next;
+	if (small->next != NULL)  /*Not the last node*/
+		small->next->prev = *big; /*node3 point back to node1*/
+
+	small->prev = (*big)->prev;  /*point to node b4 node1*/
+	small->next = *big;
+
+	if ((*big)->prev != NULL) /*not the first node (head)*/
+		(*big)->prev->next = small; /*node0 point to small*/
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = small;
+	(*big)->prev = small;
+	*big = small->prev;
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
+ * insertion_sort_list - unsing Insertion Sort, sort a doubly linked
+ * list of integers.
  *
- * Description: Prints the list after each swap.
+ * @list: a pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+    listint_t *ptr, *insert;
+	listint_t *node; /*Keeps track of the current node*/
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+    /*List is empty*/
+	if (list == NULL || *list == NULL || (*list)->next == NULL) /*or one node*/
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (node = (*list)->next; node != NULL; node = ptr)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		ptr = node->next;
+		insert = node->prev;
+		while (insert != NULL && node->n < insert->n)
 		{
-			swap_nodes(list, &insert, iter);
+			nodes_switch(list, &insert, node);
 			print_list((const listint_t *)*list);
 		}
 	}
